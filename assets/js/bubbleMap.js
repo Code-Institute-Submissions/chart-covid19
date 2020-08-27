7b1e11298da8211cba3d95ef41a6af1022167721
NgiapPuoyKoh <ngiappuoykoh@gmail.com>
2020-08-27 20:12:45 +0000

    Feature Map Refactor loadAndProcessData module

--- assets/js/bubbleMap.js
+++ assets/js/bubbleMap.js
@@ -7,6 +7,7 @@
 // const height = document.body.clientHeight;
 
 import { readData } from "./loadData.js";
+import { loadAndProcessData } from "./loadAndProcessData.js";
 
 const svg = d3.select("svg");
 
@@ -24,39 +25,7 @@ svg.call(
   })
 );
 
-Promise.all([
-  d3.csv(
-    "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv"
-  ),
-  d3.json(
-    "https://cdn.jsdelivr.net/npm/us-atlas@3.0.0/counties-albers-10m.json"
-  ),
-]).then(([covid19Data, topoJSONdata]) => {
-  console.log(covid19Data);
-  console.log(topoJSONdata);
-
-  //   build a reference table covid19 row county name using fips
-  //   const countyName = {};
-  //   covid19Data.forEach((d) => {
-  //     countyName[d.id] = d.id;
-  //   });
-
-  const covidByFips = covid19Data.reduce((accumulator, d) => {
-    // accumulator[d.id] = d.id;
-    accumulator[d.fips] = d;
-    return accumulator;
-  }, {}); //initial value of accumulator
-
-  const counties = topojson.feature(
-    topoJSONdata,
-    topoJSONdata.objects.counties
-  );
-
-  //attach covid19 row and assign it onto counties
-  counties.features.forEach((d) => {
-    Object.assign(d.properties, covidByFips[d.id]);
-  });
-
+loadAndProcessData().then((counties) => {
   //data join
   //append
   g.selectAll("path")
@@ -65,6 +34,7 @@ Promise.all([
     .append("path")
     .attr("class", "counties")
     .attr("d", pathGenerator)
+    .attr("fill", "aqua")
     .append("title")
     .text((d) => d.properties.name);
   // .text((d) => console.log(d.id));
